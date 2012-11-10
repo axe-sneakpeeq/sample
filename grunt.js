69f62c6b7ab04b2f7002c649285e0bb46d924ab1
Parasuraman <parashu@sneakpeeq.com>
2012-11-09 20:23:54 -0800

    Corrected casng of sauceKey

--- grunt.js
+++ grunt.js
@@ -32,7 +32,7 @@ module.exports = function(grunt){
 	
 	grunt.registerTask('test', function(){
 		grunt.log.writeln("Travis Secure Env:" + process.env.TRAVIS_SECURE_ENV_VARS);
-		grunt.log.writeln("saucekey:" + process.env.saucekey);
+		grunt.log.writeln("saucekey:" + process.env.sauceKey);
 	});
 	grunt.registerTask('test1', 'server saucelabs-qunit');
 	grunt.registerTask('default', 'test');
